438abe99a02f7b7fb8d284fc6add8e755ba6ae75
Victor Agbeo <vicagbeo@outlook.com>
2023-07-11 19:40:38 -0500

    structure

--- mb8-/ViewController.swift
+++ mb8-/ViewController.swift
@@ -10,12 +10,27 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var askButtonPressed: UIButton!
+    @IBOutlet weak var askButtonPressed: UIButton!;
+    let ballArr = [ #imageLiteral(resourceName: "ball1"),#imageLiteral(resourceName: "ball2"),#imageLiteral(resourceName: "ball3"),#imageLiteral(resourceName: "ball4"),#imageLiteral(resourceName: "ball5"),#imageLiteral(resourceName: "pool")]
+                   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+ 
+    
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        imageView.image = ballArr[Int.random(in: 0...5)]
+    }
 
-
+    
 }
 
+                   
+//        #imageLiteral(resourceName: "ball1")
+//        #imageLiteral(resourceName: "ball2")
+//        #imageLiteral(resourceName: "ball3")
+//         #imageLiteral(resourceName: "ball4")
+//         #imageLiteral(resourceName: "ball5")
+//         #imageLiteral(resourceName: "pool")
